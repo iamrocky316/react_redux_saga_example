1ad0e7efbf1fe20759d15d703d2960549f6d4f55
Pavithran <pavithran.r@aximsoft.com>
2019-02-17 22:56:01 +0530

    Added before API console log

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,6 +5,7 @@ import * as actionTypes from '../actions/actionTypes';
 // Sagas
 function* loadUserDetails({ payload }) {
     try {
+    console.log("[loadUserDetails] before api call")
         const user = yield call(gitHubApi, payload); // Make Api call to Github api with the username
         yield put({ type: actionTypes.LOAD_USER_SUCCESS, user }); // Yields effect to the reducer specifying the action type and optional parameter
     } catch (error) {
